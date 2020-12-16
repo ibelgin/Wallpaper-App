0372620a6fd3a5565177eb2bcd2e5700d317ccd4
Belgin Android <belginprogrammerterno@gmail.com>
2020-12-16 22:42:07 +0530

    Update FullScreen.js

--- components/FullScreen.js
+++ components/FullScreen.js
@@ -12,6 +12,7 @@ import {
   TouchableOpacity,
   TextInput,
   ScrollView,
+  RefreshControl
 } from "react-native"
 
 const Dev_Height = Dimensions.get('screen').height
@@ -22,25 +23,29 @@ const client = createClient('563492ad6f917000010000010f702841514f4d11b90c1c5cedb
 
 export default class FullScreen extends React.Component{
 
-    FindImages=(query)=>{
-      client.photos.search({ query , per_page: 20 }).then(photos => {
+    FindImages=(query,page_no)=>{
+      this.setState({ refreshing : true})
+      client.photos.search({ query , per_page: 20 , page:page_no}).then(photos => {
         this.setState({ carouselItems : photos })
         this.setState({ carouselItems : this.state.carouselItems['photos'] })
+        this.setState({ refreshing :false })
       });
     }
 
-    Find_New=()=>{
-      
-    }
 
     constructor(props){
         super(props);
         this.state = {
           carouselItems:[ ],
-          query:this.props.route.params.query
+          query:this.props.route.params.query,
+          refreshing:false
       }
-      this.FindImages(this.state.query)
     }
+
+    componentDidMount(){
+      this.FindImages(this.state.query,1)
+    }
+    
   render(){
     return(
       <SafeAreaView style={styles.container}>
@@ -49,10 +54,22 @@ export default class FullScreen extends React.Component{
           columnWrapperStyle={{justifyContent: 'space-between'}}
           data={this.state.carouselItems}
           numColumns={2}
+          refreshControl={
+            <RefreshControl refreshing={this.state.refreshing} onRefresh={()=>this.FindImages(this.state.query,2)} 
+               title="Refreshing" 
+               titleColor="#FFF" 
+               colors={["gray","orange"]}
+            />
+           }
           renderItem={({item}) => {
             return (
-              <TouchableOpacity style={{height:Dev_Height-(0.7*Dev_Height),width:"48%",borderRadius:15,justifyContent:"center",alignItems:"center"}} >
-                <Image  source={{uri:item['src']['medium']}} style={{height:"95%",width:"95%",borderRadius:15}} />
+              <TouchableOpacity 
+               style={{height:Dev_Height-(0.7*Dev_Height),width:"48%",borderRadius:15,justifyContent:"center",alignItems:"center"}} 
+               onPress={()=>this.props.navigation.navigate("ImageDisplay",{
+              "id":item["id"]
+            })}
+               >
+                <Image source={{uri:item['src']['medium']}} style={{height:"95%",width:"95%",borderRadius:15}} />
               </TouchableOpacity>
             );
           }}
@@ -73,7 +90,9 @@ const styles = StyleSheet.create({
     height:Dev_Height,
     width:Dev_Width,
     justifyContent:"center",
-    alignItems:"center"
+    alignItems:"center",
+    backgroundColor:"#222222",
+    paddingTop:StatusBar.currentHeight
   },
   FlatList_Container:{
     height:"100%",
