9653a0ccb27c845f90d316f0f380eea8229a358e
Belgin <belginprogrammerterno@gmail.com>
2021-11-09 20:05:05 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import 'react-native-gesture-handler';
 import * as React from 'react';
+import 'react-native-gesture-handler';
+
 import {NavigationContainer} from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
